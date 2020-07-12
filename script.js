a3c98580cb1082aa213ea1d6ee8c46a69088343b
Maynard Peralta <maynperalta@gmail.com>
2020-07-11 19:47:37 -0600

    fixed js file

--- script.js
+++ script.js
@@ -9,28 +9,18 @@ var temp = [];
 var pass = "";
 
 function generatePassword() {
-  var x = prompt(
-    "How many characters would you like your password to be (between 8 and 128)?"
-  );
-
+  var x = prompt("How many characters would you like your password to be (between 8 and 128)?");
   var long = parseInt(x);
-
-  // if(long < 8 || long > 128){
-  //   generatePassword()
-  // }
 
   while (long < 8 || long > 128) {
     alert("Your password must be between 8 and 128 characters!");
     long = prompt("How many characters would you like your password to be (between 8 and 128)?");
 
-
     if (long >= 8 && long <= 128) {
       console.log("Your password will be " + long + " characters in length.");
+      alert("Your password will be " + long +" characters in length.");
     }
-
-  }// end while loop
-
-  // console.log("Your password will be " + long + " characters in length.");
+  }
 
   var numConfirm = confirm("Would you like to include numbers in your password?");
 
@@ -68,9 +58,8 @@ function generatePassword() {
     alert("Your password will not include lowercase letters.");
     console.log("password will not include lowercase letters.");
   }
-  var charConfirm = confirm(
-    "Would you like to include special characters (?_+!=-@#$%&*) in your password?"
-  );
+
+  var charConfirm = confirm("Would you like to include special characters (?_+!=-@#$%&*) in your password?");
   console.log(charConfirm);
   if (charConfirm) {
     alert("Your password may include special characters.");
@@ -82,13 +71,12 @@ function generatePassword() {
   }
 
   if(charConfirm ===  false && lowerConfirm ===  false &&  upperConfirm ===  false && numConfirm ===  false ){
-
     alert("You must select at least one character category. Please try again.")
     return
-
   }
+
   gen(temp, long);
-}//end generatePassword() fct def
+}
 
   function gen(array, passwordLen) {
     for (var i = 0; i < passwordLen; i++) {
@@ -99,17 +87,12 @@ function generatePassword() {
   }
 
 
- 
-//check for the options
-//create temporary array and push array based on user's option selection
-//generate random number to pick character from temporary array
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
   document.querySelector("#password").innerHTML = pass;
-  // passwordText.innerHTML = password;
 }
 
 // Add event listener to generate button
